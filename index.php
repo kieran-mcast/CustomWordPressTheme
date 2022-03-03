330f8b30e67bc13879aaf7369c46f7b2c51c6a15
kieran-mcast <kieran.camilleri.b31252@mcast.edu.mt>
2022-03-03 10:50:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+    <?php if (have_posts()){ ?>
+
+        <?php while(have_posts()) {?>
+            <?php the_post(); ?>
+            <h2>
+                <a href ="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h2>
+            <div>
+                Posted on 
+                <a 
+                    href ="<?php the_permalink(); ?>">
+                    <?php echo get_the_date(); ?>
+                </a>
+                by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
+                    <?php echo get_the_author(); ?>
+            </a>
+            </div>
+
+            <div>
+                <?php the_excerpt(); ?>
+
+            </div>
+            <a href="<?php the_permalink(); ?>">
+                Read more
+            </a>
+        <?php } ?>
+
+    <?php } else { ?>
+        <p>Sorry, no posts match your criteria.</p>
+    <?php } ?>
+
+
+<?php get_footer(); ?>
